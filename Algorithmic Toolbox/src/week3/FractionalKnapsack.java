60cccad45137b175ebe227e914564f63421e1dd3
Administrator <Administrator@B2R8YULS5QMJO1C>
2016-03-19 23:02:02 +0700

    f

---
+++ Algorithmic Toolbox/src/week3/FractionalKnapsack.java
@@ -0,0 +1,29 @@
+package week3;
+import java.util.Scanner;
+
+public class FractionalKnapsack {
+    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
+        double value = 0;
+        int[]a = new int[values.length];
+        //write your code here
+        while(capacity>0){
+        	if(capacity==0) return value;
+        	
+        	capacity--;
+        }
+        return value;
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int capacity = scanner.nextInt();
+        int[] values = new int[n];
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+            weights[i] = scanner.nextInt();
+        }
+        System.out.println(getOptimalValue(capacity, values, weights));
+    }
+} 
