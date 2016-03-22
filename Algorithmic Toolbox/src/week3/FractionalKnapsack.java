b73e48d4662f853c39b45160593cad511308648f
Lai Trach <trach.lai@outlook.com>
2016-03-22 22:11:40 +0700

    this ex 2

--- Algorithmic Toolbox/src/week3/FractionalKnapsack.java
+++ Algorithmic Toolbox/src/week3/FractionalKnapsack.java
@@ -3,15 +3,52 @@ import java.util.Scanner;
 
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
-        double value = 0;
-        int[]a = new int[values.length];
-        //write your code here
-        while(capacity>0){
-        	if(capacity==0) return value;
-        	
-        	capacity--;
-        }
-        return value;
+    	double temp;
+    	int size = values.length;
+    	if(capacity ==0) return 0;
+    	double[] profits = new double[size];
+    	
+    	
+    	for(int i =0; i < size; i++){
+    		profits[i] = (double)values[i]/weights[i];
+    	}
+    	for(int i =0; i < size-1; i++){
+    		for(int j = i+1; j < size; j++){
+    			if(profits[j] > profits[i]){
+    				temp = profits[j];
+    				profits[j] = profits[i];
+    				profits[i] = temp;
+    				
+    				temp = weights[j];
+    				weights[j] = weights[i];
+    				weights[i] = (int)temp;
+    				
+    				temp = values[j];
+    				values[j] = values[i];
+    				values[i] = (int)temp;
+    			}
+    		}
+    	}
+    	double currentSum = 0.0; int k=0; 
+    	int m = 0; int cap =0;
+
+		for(k =0; k < size; k++){
+			cap =0;
+			while(m <= capacity){
+				m +=1; cap++;
+			
+				//if(m >= capacity) break;
+				
+				currentSum += profits[k];
+				
+				if(m==capacity) return currentSum;
+				if(cap == weights[k]) break;
+			}
+
+    	}
+		
+    	
+        return currentSum;
     }
 
     public static void main(String args[]) {
@@ -26,4 +63,5 @@ public class FractionalKnapsack {
         }
         System.out.println(getOptimalValue(capacity, values, weights));
     }
+    
 } 
